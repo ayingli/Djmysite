e443a3582618c0a18cac45828d5046e3afc779ce
ayingli <liyysap@126.com>
2018-04-19 14:41:04 +0800

    URL

--- learning/admin.py
+++ learning/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 from learning.models import Topic
+from learning.models import Topic,Entry
 admin.site.register(Topic)
+admin.site.register(Entry)
 
 import learning.models
 admin.site.site_title='第一个网站'
